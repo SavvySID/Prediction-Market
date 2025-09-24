f8a0b4c60513525bb34fc9a20e3f8e6858f520b4
SavvySID <siddhant3187@gmail.com>
2025-09-24 12:08:12 +0530

    Add scripts/deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const hre = require("hardhat");
+
+async function main() {
+  const PredictionMarket = await hre.ethers.getContractFactory("PredictionMarket");
+  const contract = await PredictionMarket.deploy();
+  await contract.waitForDeployment();
+  console.log("Contract deployed to:", await contract.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
